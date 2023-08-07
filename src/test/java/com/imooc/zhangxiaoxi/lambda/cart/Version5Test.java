5072ba12b415ca6e27504d606754bb810329e7f0
zhaojiacheng <zhaomo0823@gmail.com>
2023-08-07 11:21:36 +0800

    Initial commit

---
+++ src/test/java/com/imooc/zhangxiaoxi/lambda/cart/Version5Test.java
@@ -0,0 +1,22 @@
+package com.imooc.zhangxiaoxi.lambda.cart;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+
+import java.util.List;
+
+public class Version5Test {
+
+    @Test
+    public void filterSkus() {
+        List<Sku> cartSkuList = CartService.getCartSkuList();
+
+        // 过滤商品单价大于1000的商品
+        List<Sku> result = CartService.filterSkus(
+                cartSkuList, sku -> sku.getSkuPrice() > 1000);
+
+        System.out.println(JSON.toJSONString(
+                result, true));
+    }
+
+}
